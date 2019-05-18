4c1997f4d22ce790f9d2c1bfd57ea48bc9fe6cd9
Nakmak98 <Nakmak1998@gmail.com>
2019-05-19 02:53:06 +0300

    add .gitignore

---
+++ fdm_bar.c
@@ -0,0 +1,5 @@
+//
+// Created by nakmak98 on 19.05.19.
+//
+
+#include "fdm_bar.h"
---
+++ fdm_bar.h
@@ -0,0 +1,8 @@
+//
+// Created by nakmak98 on 19.05.19.
+//
+
+#ifndef MMAPD_FDM_BAR_H
+#define MMAPD_FDM_BAR_H
+
+#endif //MMAPD_FDM_BAR_H
