bac83c563937b7e8de3db15e168f185fc495504d
Nakmak98 <Nakmak1998@gmail.com>
2019-05-25 00:43:37 +0300

    Delete old files

--- fdm_bar.c
+++
@@ -1,5 +0,0 @@
-//
-// Created by nakmak98 on 19.05.19.
-//
-
-#include "fdm_bar.h"
--- fdm_bar.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by nakmak98 on 19.05.19.
-//
-
-#ifndef MMAPD_FDM_BAR_H
-#define MMAPD_FDM_BAR_H
-
-#endif //MMAPD_FDM_BAR_H
